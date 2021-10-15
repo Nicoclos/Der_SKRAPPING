5ffbbe9ea56049f3606729574e2eae4daad57bf4
nicoclos <n.bia@outlook.fr>
2021-10-15 02:04:49 +0200

    Nicoclos - Der SKRAPPING - PROCESSING - v2

---
+++ spec/dark_trader_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/dark_trader'
+require 'nokogiri'   
+require 'open-uri'
+page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/"))
+
+
+describe "the program get data" do
+  it " return crypto's symbol and crypto is not nil" do
+    expect(crypto_price(page)).not_to be_nil
+  end
+end  
+describe "the program get data" do
+  it " return crypto's symbol and crypto is not nil" do
+    
+    expect(crypto_name(page)).not_to be_nil
+  end
+end  
