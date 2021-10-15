60c7968d865f7c14a83352f5cf16e79c02e62b20
nicoclos <n.bia@outlook.fr>
2021-10-15 02:26:52 +0200

    Nicoclos - Der SKRAPPING - PROCESSING - v3

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -2,27 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 
 page= Nokogiri::HTML(URI.open("https://coinmarketcap.com/"))
+
+# scraping name crypto
 def crypto_name(page)
-    # scraping name crypto
-    name= page.xpath('//tr[*]/td[3]').collect(&:text)
-    i=0
-    j=1
-    while i<name.length do
-      j=j.to_s
-      name[i]= name[i].gsub("Buy","")
-      name[i] = name[i].gsub(j,"")
-      j = j.to_i
-      j+=1
-      i+=1
-    end
-    name
-  end
+  name= page.xpath('//tr[*]/td[3]').collect(&:text)
+end
   
-  def crypto_price(page)
-    # scraping price crypto
-   page.xpath('//tr[*]/td[4]').collect(&:text)
-  end
-  page= Nokogiri::HTML(URI.open("https://coinmarketcap.com/"))
-  name=crypto_name(page)
-  price = crypto_price(page)
-  puts tableau = Hash[name.zip(price)]
+# scraping price crypto
+def crypto_price(page)
+  page.xpath('//tr[*]/td[4]').collect(&:text)
+end
+
+name=crypto_name(page)
+price = crypto_price(page)
+puts tableau = Hash[name.zip(price)]
--- spec/dark_trader_spec.rb
+++ spec/dark_trader_spec.rb
@@ -9,9 +9,9 @@ describe "the program get data" do
     expect(crypto_price(page)).not_to be_nil
   end
 end  
+
 describe "the program get data" do
   it " return crypto's symbol and crypto is not nil" do
-    
     expect(crypto_name(page)).not_to be_nil
   end
 end  
